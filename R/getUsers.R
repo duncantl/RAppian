635e56b1ba3042f66d74d6798df6ff918a45b795
Duncan Temple Lang <duncan@r-project.org>
2023-08-26 12:30:34 -0700

    start of a package to query Appian information when it is not easily available to export, at least based on my limited familiarity with the UI

---
+++ R/getUsers.R
@@ -0,0 +1,22 @@
+getAppianLogins =
+function(app.cookie = cookie("appian.cookie"), verbose = FALSE)
+{
+    u = "https://ucdavistest.appiancloud.com/suite/rest/a/applications/latest/app/admin/page/users"
+# Cookie was last added and the necessary element
+    js = getURLContent(u, httpheader = c(
+                          "X-Appian-features" = "7ffceebc",
+                          "X-APPIAN-CSRF-TOKEN" = "0bf74a5b-a190-4bd9-b649-6252c160ccd6"
+#                          "x-appian-suppress-www-authenticate" ="true",
+#                          "X-Client-Mode" = "ADMIN",
+#                          "X-Appian-Ui-State" = "stateful",
+#                          "X-Appian-Initial-Form-Factor" = "DESKTOP",
+#                          "X-Appian-Features-Extended" = "1dbff7f49dc1fffceebc"
+                          ),
+                   referer = "https://ucdavistest.appiancloud.com/suite/admin/page/users",
+                   cookie = app.cookie,
+                   useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/117.0",
+                   verbose = verbose)
+ans = fromJSON(rawToChar(js))
+
+ans$ui$centerPaneContent$contents[[1]]$contents[[1]]$columns[[1]]$contents[[2]]$columns[[2]]$data
+}
