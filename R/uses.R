a5b313b571df5d385af90235f3e2feae8f2a9fca
Duncan Temple Lang <duncan@r-project.org>
2023-09-10 09:55:26 -0700

    compute top-level uuids from file names of objects. Then find uuids in each file and build the connection graph

---
+++ R/uses.R
@@ -0,0 +1,26 @@
+#
+# For each object, find which 
+#
+# Should we be working from the SAIL code or from the XML
+#
+#
+
+if(FALSE) {
+    af = list.files(recursive = TRUE, pattern = "\\.xml$")
+    top = toplevelUUIDs(af = af)
+    us = lapply(af, uses, toplevel = top)
+}
+
+
+uses =
+function(file, txt = paste(readLines(file), collapse = "\n"),
+          toplevel = toplevelUUIDs())
+{
+    regmatches(txt, gregexpr(uuidRX3, txt))[[1]]
+}
+
+toplevelUUIDs =
+function(dir = ".", af = list.files(dir, recursive = TRUE, pattern = "\\.xml$"))
+{
+    gsub("\\.xml$", "", basename(af))
+}
