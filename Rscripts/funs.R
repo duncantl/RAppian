47b1fef808c721486dc21e794ecc4e03ddd134b2
Duncan Temple Lang <duncan@r-project.org>
2023-09-12 17:23:46 -0700

    cleanup the code that gets the top-level summary and resolve functions and all symbols

--- Rscripts/funs.R
+++ Rscripts/funs.R
@@ -1,8 +1,11 @@
 library(RAppian)
 
+library(CodeAnalysis)
+
 dir = "."
 code = mkCodeInfo(dir)
 rcode = lapply(code$code, StoR, TRUE)
+names(rcode) = code$name
 
 map = mkSummary()
 #umap = mkUUIDMap(dir)
@@ -14,13 +17,22 @@ funs2 = lapply(funs, mapName, map)
 showCounts(dsort(table(unlist(funs2))))
 
 
-
+# Get all the symbols in all of the functions and then resolve the
+# UUIDs, urns, etc.
 rsyms = lapply(rcode, CodeAnalysis:::all_symbols)
-tt = table(unlist(rsyms))
-u = grep("#urn", names(tt), value = TRUE)
 
-m2 = mapName(names(tt) , map)
+if(FALSE) {
+    # some of the syms map to the same name so setting the names on tt and using showCounts() (which creates a data.frame)
+    # raises an error about duplicate names.
 
-recTypes = recordTypeList()
+    tt = table(unlist(rsyms))
+    m2 = mapName(names(tt) , map)    
+    names(tt) = m2
+    showCounts(dsort(tt))
+}
 
-
+table(duplicated(m2))
+asyms = unlist(rsyms)
+masyms = structure(mapName(unique(asyms), map), names = unique(asyms))
+tt = table(masyms[asyms])
+showCounts(dsort(tt))
