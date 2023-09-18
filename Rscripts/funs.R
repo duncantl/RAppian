1af5b3bb21c47d2a18d695bfa0066cebe38c0f52
Duncan Temple Lang <duncan@r-project.org>
2023-09-17 18:54:18 -0700

    more updates now that we have rcode2, but need to rationalize this

--- Rscripts/funs.R
+++ Rscripts/funs.R
@@ -6,40 +6,71 @@ invisible(lapply(list.files("~/OGS/EForms/RAppian/R", full = TRUE, pattern = "\\
 dir = "."
 #umap = mkUUIDMap(dir)
 map = mkSummary()
+map$code = sapply(map$file, getCode)
 code = mkCodeInfo(dir)
 rcode = lapply(code$code, function(x) try(StoR(x, TRUE)))
 names(rcode) = code$name
 rcode2 = lapply(rcode, function(x) try(rewriteCode(x, map)))
 err = sapply(rcode2, inherits, 'try-error')
+stopifnot(!any(err))
 
 
 
 # Assumes names, not calls, but should be good for SAIL. Or did I see one expression in there?
-funs = lapply(rcode, function(x) sapply(findCallsTo(x), function(x) as.character(x[[1]])))
+funs = lapply(rcode2, function(x) sapply(findCallsTo(x), function(x) as.character(x[[1]])))
 
-funs2 = lapply(funs, mapName, map)
-tt = dsort(table(unlist(funs2)))
+#funs2 = lapply(funs, mapName, map)
+tt = dsort(table(unlist(funs)))
 showCounts(tt)
 
 # Get all the symbols in all of the functions and then resolve the
 # UUIDs, urns, etc.
-rsyms = lapply(rcode, CodeAnalysis:::all_symbols)
+# Can use rcode2, but not getting same answers - big differences
+rsyms = lapply(rcode, ruses)
 
 if(FALSE) {
     # some of the syms map to the same name so setting the names on tt and using showCounts() (which creates a data.frame)
     # raises an error about duplicate names.
 
     tt = table(unlist(rsyms))
-    m2 = mapName(names(tt) , map)    
+    m2 = mapName(names(tt) , map)
+    table(duplicated(m2))
+    # 12 duplicated and these are NA and "NA -> NA"
     names(tt) = m2
     showCounts(dsort(tt))
 }
 
-table(duplicated(m2))
+#
 asyms = unlist(rsyms)
 masyms = structure(mapName(unique(asyms), map), names = unique(asyms))
 tt = table(masyms[asyms])
 showCounts(dsort(tt))
+
+
+
+calls = lapply(rcode2, function(x) getGlobals(x)$functions)
+calls = calls[ sapply(calls, length) > 0]
+
+tt = table(unlist(calls))
+showCounts(dsort(tt))
+omit = c("(", ">", "<", "$", "<=", ">=", "==", "&", "[", "-", "+", ":")
+
+sysFuns = setdiff(names(tt)[ (grepl("^a!", names(tt)) | !grepl("!", names(tt))) & !grepl("^#", names(tt))], omit)
+
+ldefs = names(tt) %in% map$name
+
+
+
+library(igraph)
+calls2 = lapply(calls, function(x) { x = gsub("^(rule|interface|recordType)!", "", x); gsub("^EFRM_([A-Z]+_)?", "", unique(x[ x %in% map$name ])) })
+m = cbind( rep(names(calls2), sapply(calls2, length)), unlist(calls2))
+g = igraph::graph_from_edgelist(m)
+plot(g, margin = rep(0.1, 4), vertex.shape = "none", vertex.label.cex = 0, edge.arrow.mode = 0, layout = layout.drl)
+#plot(g, vertex.label.cex = 0, edge.arrow.mode = 0)
+
+
+
+#######################
 
 
 # Constants
@@ -96,6 +127,7 @@ map$codeUses = getCodeUses(map, map$code)
 
 
 
+if(FALSE) {
 # This is uses from the entire XML file, not just the code.
 map$uses = lapply(map$file, uses, toplevel = map$uuid)
 map$uses2 = lapply(map$uses, function(x) unique(map$name[ match(x, map$uuid) ]))
@@ -107,4 +139,6 @@ library(igraph)
 m = cbind( rep(map$name, sapply(map$uses2, length)), unlist(map$uses2))
 g = igraph::graph_from_edgelist(m)
 plot(g, vertex.label.cex = 0, edge.arrow.mode = 0)
+}
+
 
