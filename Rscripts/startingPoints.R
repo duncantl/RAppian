a003b4a86dd3f21bfee3d11ec9a8d91c9cc24596
Duncan Temple Lang <duncan@r-project.org>
2023-09-23 11:02:07 -0700

    compute starting points and rule inputs and process variables for the different forms on the landing page

---
+++ Rscripts/startingPoints.R
@@ -0,0 +1,22 @@
+
+k = rcode2$EFRM_getWorkflowFormRelatedInformationBasedOnVisibility$"local!allFormMap"
+
+wkflows = lapply(k[-1], function(x) x[ - 1])
+names(wkflows) = sapply(wkflows, function(x) x[[1]])
+
+ # remove the SBrace
+forms = lapply(wkflows, function(x) x$forms[-1])
+f2 = unlist(lapply(forms, as.list))
+info = data.frame(name = sapply(f2, function(x) x$name),
+                  processModel = sapply(f2, function(x) getName(x$processModel))
+                  )
+
+info$procModel = sapply(info$processModel, function(x) getName(uuid2File(getConstantInfo(map$file[x == map$name])$value)))
+info$params = lapply(f2, function(x) names(x$processParameters)[-1])
+
+
+# Too recursive.
+#findCallsTo(k, "a!map", parse = FALSE)
+
+
+################
