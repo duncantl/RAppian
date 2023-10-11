1a7b02b9d7b0ffca40601145d30a3f6b41e32615
Duncan Temple Lang <duncan@r-project.org>
2023-10-10 18:59:38 -0700

    code to create data.frames (and then a spreadsheet) of counts of symbols and types for an app

---
+++ R/tableToDF.R
@@ -0,0 +1,42 @@
+table2df =
+function(x, map = NULL)
+{
+    if(length(x) == 1)
+        ans = data.frame(name = names(x), count = as.integer(x))
+    else        
+        ans = data.frame(name = rownames(x), count = as.integer(x))
+
+    if(!is.null(map)) {
+        tmp = gsub(".*!", "",  ans$name)
+        m = match(tmp, map$name, 0)
+        ans$description = ""
+        if(any(m != 0)) 
+            ans$description[m != 0] = map$description[m]
+        
+    }
+
+    ans
+}
+
+mkCountDfs =
+function(map, syms)
+{
+    tmp2 = list("Overall Type Count" =  table2df(dsort(table(map$type))))
+    
+    tt = dsort(table(unlist(syms)))
+    w = grepl("^SYSTEM_SYSRULES_", names(tt))
+    names(tt)[w] = gsub("^SYSTEM_SYSRULES_", "a!", names(tt)[w])
+    
+    category = c(rule = "rule!", constant = "constant!",
+                 interface = "interface!",
+                 integration = "outboundIntegration!",
+                 processModel = "pm!",             
+                 recordType = "recordType!",
+                 system = "a!", fn = "fn!", fv = "fv!")
+    tmp2[names(category)] = lapply(paste0("^", category), function(rx) table2df( tt[ grep(rx, names(tt) )], map))
+
+    tmp2
+}
+
+
+       
