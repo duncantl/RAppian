0c20398924de6a96ba608af3f4dd43c5c9590138
Duncan Temple Lang <duncan@r-project.org>
2023-12-27 08:00:48 -0800

    convert the columns whose name match _ON$ regex to POSIXct. Assume GMT as the base and then convert to local time zone by default. Done for all tables

--- R/dbms.R
+++ R/dbms.R
@@ -1,6 +1,11 @@
+# The interface to the databases in appian is phpMyAdmin
+# https://docs.phpmyadmin.net/en/latest/
+# https://docs.phpmyadmin.net/en/latest/config.html
+
 readDBDump =
     # file can be the actual JSON from dbDump()
-function(file = mostRecent("127_0_0_1.*\\.json$", dir), dir = ".", removePrefix = TRUE, efrmOnly = TRUE)
+function(file = mostRecent("127_0_0_1.*\\.json$", dir), dir = ".", removePrefix = TRUE, efrmOnly = TRUE,
+         convertTimestamps = c(GMT = ""))
 {
     j = RJSONIO::fromJSON(file)
     w = sapply(j, `[[`, "type") == "table"
@@ -23,7 +28,11 @@ function(file = mostRecent("127_0_0_1.*\\.json$", dir), dir = ".", removePrefix 
     if(removePrefix)
         ids = gsub("^(EFRM|CMN|RWM|EXP|[A-Z]+)_", "", ids)
     
-    structure(lapply(j[i], mkDBTable), names = ids)
+    ans = structure(lapply(j[i], mkDBTable), names = ids)
+    if(length(convertTimestamps) && !all(is.na(convertTimestamps)))
+        ans = lapply(ans, cvtTimestamps, tz = convertTimestamps)
+
+    ans
 }
 
 mkDBTable =
@@ -49,6 +58,39 @@ function(x)
     # simplify2array(x)
     type.convert(x, as.is = TRUE)
 }
+
+# Which columns across all the tables have any entries that start with 2023-
+# table(unlist(sapply(d[sapply(d, nrow) > 0], function(df) names(df)[sapply(df, function(x) any(grepl("^2023-", x)))])))
+# all the _ON$
+# ACTUAL_EXAM_DATE
+# COMPLETION_DATE
+# DUE_DATE_TIME
+# FINAL_EXAM_DATE
+# QE_PASS_DATE
+# SCHEDULED_QE_DATE
+# TERM_END
+# TERM_FILING_DEADLINE
+# TERM_LAST_DAY_INSTRN
+# TERM_START
+
+cvtTimestamps =
+function(df, cols = NA, tz = c(GMT = ""))
+{
+    if(is.na(cols)) 
+        cols =  grep("_ON$", names(df))
+
+    if(length(cols))
+        df[cols] =  lapply(df[cols], cvtTimestampColumn, tz = tz)
+    
+    df
+}
+
+cvtTimestampColumn =
+function(x, tz = c(GMT = ""))
+{
+   .POSIXct(as.POSIXct(x, tz = names(tz)), tz)
+}
+
 
 
 
