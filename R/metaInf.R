a5fbd42b450d389b26a3743915a3007e468884b8
Duncan Temple Lang <duncan@r-project.org>
2024-03-04 08:24:50 -0800

    functions to read export.log and design guidance and make them more interpretable

---
+++ R/metaInf.R
@@ -0,0 +1,32 @@
+readExportLog =
+function(file = file.path(metaInfDir(dir), "export.log"), ll = readLines(file), dir = ".")
+{
+    w = grepl(":$", ll)
+    g = split(ll, cumsum(w))
+    
+    ids = sapply(g, `[`, 1)
+    g = lapply(g, function(x) x[-1])
+    names(g) = trimws(gsub("\\([0-9,]+\\)$", "", gsub(":$", "", ids)))
+    g
+}
+
+guidance =
+function(map = mkSummary(), file = file.path(metaInfDir(dir), "design-guidance.json"), dir = ".")
+{
+    dg = fromJSON(file)
+    df = do.call(rbind, lapply(dg, as.data.frame))
+
+    m = match(df$objectUuid, map$uuid)
+    df$name = map$name[m]
+
+    df$objectTypeName = gsub("^\\{http://www.appian.com/ae/types/2009\\}", "", df$objectTypeName)
+
+    vars = c("name", "designGuidanceKey", "dismissed", "instanceCount", "objectTypeName", "objectUuid")
+    df[, vars]
+}
+
+metaInfDir =
+function(dir = ".")
+{
+    file.path(path.expand(dir), "META-INF")
+}
