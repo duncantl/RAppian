16810414155af69205eec48ba6360bc3089bfe69
Duncan Temple Lang <duncan@r-project.org>
2024-03-30 18:17:38 -0700

    more refined way to get the combined data.frame of mkSummary() maps for the instances

--- Rscripts/compareInstances.R
+++ Rscripts/compareInstances.R
@@ -1,18 +1,16 @@
+#
+# This is a way to compare the different instances such as to find which have
+# + different UUIDs for the same object/name
+# + an object on only one, or not on all three. 
+#
+# This is different from running basics.R and then mkSummary() on the other 2 instances.
+# basics.R drops the CMN, specifically CMN_ucAnyTypeArrayPickerFilter.
+#
+
+
 dirs = c(dev = "../March30Dev2Test_v2", test = "~/OGS/EForms/PhaseI/Deploy/March30TestExport/", prod = ".")
 
 maps = lapply(dirs, mkSummary)
 ma = do.call(rbind, maps)
 ma$instance = rep(names(maps), sapply(maps, nrow))
 
-
-
-if(FALSE) {
-#source("~/OGS/EForms/RAppian/Rscripts/basics.R")
-# or 
-prod = mkSummary()
-dev = mkSummary("../March30Dev2Test_v2")
-test = mkSummary("~/OGS/EForms/PhaseI/Deploy/March30TestExport/")
-ma = rbind(dev, test, prod[, names(dev)])
-ma$instance = rep(c("dev", "test", "prod"), sapply(list(dev, test, prod), nrow))
-maps = list(dev = dev, test = test, prod = prod)
-}
