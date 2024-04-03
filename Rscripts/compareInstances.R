8ae5241725f420c3bccc93ad27ee38bdc6348e5d
Duncan Temple Lang <duncan@r-project.org>
2024-04-02 21:30:17 -0700

    more on comparing objects in different Appian instances

--- Rscripts/compareInstances.R
+++ Rscripts/compareInstances.R
@@ -7,10 +7,30 @@
 # basics.R drops the CMN, specifically CMN_ucAnyTypeArrayPickerFilter.
 #
 
-
+library(RAppian)
 dirs = c(dev = "../March30Dev2Test_v2", test = "~/OGS/EForms/PhaseI/Deploy/March30TestExport/", prod = ".")
 
 maps = lapply(dirs, mkSummary)
 ma = do.call(rbind, maps)
 ma$instance = rep(names(maps), sapply(maps, nrow))
+
+
+nuuid = tapply(ma$uuid, ma$name, function(x) length(unique(x)))
+table(nuuid)
+
+
+ww = ma$name %in% names(nuuid)[w]
+g = split(ma[ww,], ma$name[ww])
+
+
+
+########
+# Objects that are not in all instances
+
+ninst = tapply(ma$instance, ma$name, function(x) length(unique(x)))
+names(ninst)[ninst == 1]
+ma[ ma$name %in% names(ninst)[ninst == 1], ]
+
+
+names(ninst)[ninst == 2]
 
