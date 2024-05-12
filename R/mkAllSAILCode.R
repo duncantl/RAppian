9786c57ca4e9922afa31e2560f7b67be8134b79a
Duncan Temple Lang <duncan@r-project.org>
2024-05-12 16:11:38 -0700

    get the constants and their references to PMs and add these to the call graph. also function to get the code only from a PM.

--- R/mkAllSAILCode.R
+++ R/mkAllSAILCode.R
@@ -1,3 +1,8 @@
+#
+# This is really mkAllSAILCallGraph rather than making the code.
+#
+#
+
 if(FALSE) {
     # from Rscripts/basics.R, get map and rcode2
     # Then add the information from the site, record type and process model to the call graph.
@@ -6,12 +11,13 @@ if(FALSE) {
 
 
 
-mkAllCode =
+mkAllCodeCallGraph =
+    # Really the call graph.
     #
     # Add the site, process model and record type uses of functions in the applications
     # to the call graph g.
     #
-function(map, rcode2, g = callGraph(map$name, rcode, TRUE))
+function(map, rcode2, g = callGraph(map$name, rcode, TRUE), constants = TRUE)
 {
     ty = (map$type == "processModel")
     g2 = do.call(rbind, lapply(map$file[ty], mkPMCallGraph, map, rcode2))
@@ -24,7 +30,30 @@ function(map, rcode2, g = callGraph(map$name, rcode, TRUE))
     # site
     ty = (map$type == "site")
     si = lapply(map$file[ty], siteInfoCallGraph, map, rcode2)
-    g = rbind(g, do.call(rbind, si))    
+    g = rbind(g, do.call(rbind, si))
+
+    # Could find references in constants to PMs and then where those constants
+    # are used.
+
+    if(constants)
+        g = rbind(g,
+                  pmConstantCallGraph(map),
+                  constantCallGraph(map, rcode2))
+
+    invisible(g)
+}
+
+getPMCode =
+function(doc, map)
+{
+    if(is.character(doc))
+        doc = xmlParse(mapFile(doc, map))
+
+    ci = customInputs(doc, map)$code
+    ci = ci[ sapply(ci, length) > 0 ]
+    co = customOutputs(doc, map)$code
+    co = co[ sapply(co, length) > 0 ]    
+    c(ci, co, sapply(startForm(doc), as.name))
 }
 
 
@@ -76,3 +105,37 @@ function(file, map, rcode2, name = getName(file) )
                      called = si$name))
 }
 
+
+pmConstantCallGraph =
+    #
+    # Which constants refer to process models as a calls/called data.frame
+    #
+function(map, info = getConstantInfo(map))
+{
+    tmp = pmConstants(map, info)
+    m = match(tmp$value, map$uuid)
+    data.frame(calls = tmp$name,
+               called = map$name[i])
+}
+
+pmConstants =
+function(map, info = getConstantInfo(map))
+{
+    w = info$type == "ProcessModel"
+    info[w,]
+}
+
+
+constantCallGraph =
+function(map, rcode2)
+{
+    ty = map$type == 'constant'
+    cons = map$name[ty]
+
+    syms = lapply(rcode2, getAllSymbols, FALSE)
+    syms = lapply(syms, function(x) gsub("^constant!", "", x))
+    syms2 = lapply(syms, function(x) x[ x %in% cons ])
+
+    data.frame(calls = rep(names(syms2), sapply(syms2, length)),
+               called = unlist(syms2))
+}
