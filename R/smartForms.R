48b02fc527a2a78c327669ef62cc435bbd32413e
Duncan Temple Lang <duncan@r-project.org>
2024-09-14 15:23:20 -0700

    code to read the SAIL code in which the new smart forms are described in a a!map()

---
+++ R/smartForms.R
@@ -0,0 +1,95 @@
+#
+# Get information about the "generic" smart forms.
+#
+
+getSmartFormsDescriptions =
+function(code = rcode2$EFRM_smartGenericFormInfo)
+{
+    desc = code[["local!actualForms"]]
+
+    vars = mkVars(code)
+    lapply(desc[-1], mkSmartFormDesc, vars)
+}
+
+mkSmartFormDesc =
+function(m, vars)
+{
+
+    # now replace any variables.
+    desc = as.list( m[-1] )
+}
+
+mkVars =
+    #
+    #
+    #
+function(code)    
+{
+    vars = code[ grepl("^local!", names(code) ) ]
+    for(i in seq(along.with = vars)) 
+        vars[[i]] = langReplace(vars[[i]], vars[ seq_len( i - 1L) ])
+
+    vars
+}
+
+langReplace =
+    #
+    # recursively replace symbols in x with the corresponding value from the list `what`
+    #
+function(x, what)    
+{
+
+    # recursively process any language objects that are not symbols.
+    w = sapply(x, is.symbol)
+    wl = sapply(x, is.language)
+    w2 = wl & !w
+    if(any(w2))
+        x[w2] = lapply(x[w2], langReplace, what)
+
+    # replace the symbols.
+    if(any(w)) {
+        v = x[w]
+        v = sapply(v, as.character)
+        m = match(v, names(what), 0)
+        x[w][m != 0] = what[ m ]
+    }
+    
+    x
+}
+
+
+#
+#
+replaceConstants =
+function(code, cons)    
+{
+    idx = indexWalkCode(code, isConstRef)
+    if(length(idx)) {
+        for(i in idx) {
+            #XXX could have  constant!xxx[constant!yyy] and end up replacing in wrong order.
+           code = replaceConstant(i, code, cons)
+        }
+    }
+    
+    code
+}
+
+replaceConstant =
+function(idx, code, cons)    
+{
+    p = getParent(idx, code)
+    if(isCallTo(p, "[")) {
+        e = p
+        idx = idx[-length(idx)]
+    } else
+        e = code[[idx]]
+
+    val = mapConsValue(code[[idx]], cons)
+
+    insertByIndex(val, idx, code)
+    
+}
+
+isConstRef =
+function(x, ...)   
+  is.symbol(x) && grepl("^constant!", as.character(x))
