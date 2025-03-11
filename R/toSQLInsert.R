48e6e25482183b02cd55af1d31ee3f17d240535a
Duncan Temple Lang <duncan@r-project.org>
2025-03-11 12:36:37 -0700

    removed the sqlAposQuote and sqlQuote implementations.

--- R/toSQLInsert.R
+++ R/toSQLInsert.R
@@ -25,7 +25,7 @@ function(rows, table, tableDef = NULL, requestId = NA,
     isString = grep("varchar|datetime", tableDef$Type[w])
 
     # should process other characters such as ;
-    values[isString] = sapply(values[isString], sqlQuote) # sqlAposQuote
+    values[isString] = sapply(values[isString], sqlQuote)
 
     i = which(colNames == "REQUEST_ID")
     if(length(i))
@@ -37,62 +37,6 @@ function(rows, table, tableDef = NULL, requestId = NA,
             table,
             paste(colNames, collapse = ", "),
             paste(values, collapse = ", "))
-}
-
-if(FALSE) {
-    tst = c("'abc'xyz", "Ellen Hartigan-O'Connor", "abc'")
-    sapply(tst, sqlAposQuote)
-
-    tst2 = c("'abc'xyz", "Ellen Hartigan-O'Connor", "abc'", "Science & Technology")
-    sapply(tst, sqlQuote)    
-}
-
-sqlQuote =
-function(x)    
-{
-    m = c("'" = 39, "&" = 38)
-    for(i in names(m))
-        x = RAppian:::sqlAposQuote(x, sprintf("CHAR(%d)", m[i]), i)
-
-    gsub("'+$", "'", gsub("^'+", "'", x))
-    
-#    x
-}
-
-sqlAposQuote =
-    # This is for creating a literal string or a call to CONCAT()
-    # so the result can be used in a SQL query.
-    #
-    # Turns a string into either
-    #   single-quoted, e.g., 'xyz'
-    #   call to CONCAT(), replacing ' with CHAR(39)
-    #
-    # Takes a single string for now.
-function(x, value = "CHAR(39)", char = "'")
-{
-    if(!grepl(char, x))
-        return(sprintf("'%s'", x))
-
-    # Now deal with a ' in the string and create
-    #  CONCAT( part1, CHAR(39), part2, CHAR(39), ...)
-    # But have to deal with ' at the start or the end.
-    
-    els = strsplit(x, char)[[1]]
-    atStart = grepl(paste0("^", char), x)
-    if(atStart)
-        els = els[-1]
-
-    els = sprintf("'%s'", els)    
-
-    tmp = paste(els, collapse = paste0(", ", value, ", "))
-
-    
-    if(grepl(paste0(char, "$"), x))
-        tmp = paste0(tmp, ", ", value)
-    if(atStart) 
-        tmp = paste0(value, ", ", tmp)
-    
-    sprintf("CONCAT(%s)", tmp)
 }
 
         
